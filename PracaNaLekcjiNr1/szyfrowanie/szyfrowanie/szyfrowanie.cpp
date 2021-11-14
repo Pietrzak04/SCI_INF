81f49547cf4451b94b864c1c122b3d0f42decbd0
Pietrzak04 <piotr.mijakowski04@gmail.com>
2021-11-14 13:55:11 +0100

    add main program functions

--- PracaNaLekcjiNr1/szyfrowanie/szyfrowanie/szyfrowanie.cpp
+++ PracaNaLekcjiNr1/szyfrowanie/szyfrowanie/szyfrowanie.cpp
@@ -1,20 +1,190 @@
-﻿// szyfrowanie.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿// program szyfrujący
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+string substitution(string t, int k)
+{
+	int l = t.length();
+
+	if (!(k >= 1 && k <= 26))
+	{
+		cout << "niewłaściwy klucz" << endl;
+		cout << "tekst nie został zmieniony" << endl;
+
+		return t;
+	}
+
+	//iterate string array
+	for (int i = 0; i < l; i++)
+	{
+		//check if the current char is in the uppercase range
+		if (t[i] >= 'A' && t[i] <= 'Z')
+		{
+			//check if char doesn't need to be transferred 
+			if (t[i] + k <= 'Z')
+			{
+				t[i] += k;
+			}
+			else {
+				t[i] = t[i] + k - 26;
+			}
+		}
+		//check if the current char is in the lowercase range
+		else if (t[i] >= 'a' && t[i] <= 'z')
+		{
+			//check if char doesn't need to be transferred 
+			if (t[i] + k <= 'z')
+			{
+				t[i] += k;
+			}
+			else {
+				t[i] = t[i] + k - 26;
+			}
+		}
+		//if the current char is out of range do nothing
+	}
+
+	return t;
+}
+
+string transposition(string t)
+{
+	int l = t.length();
+
+	//iterate string array
+	for (int i = 0; i < l; i += 2)
+	{
+		//break if length is odd
+		if (i + 1 == l)
+		{
+			break;
+		}
+
+		//swap two elements
+		swap(t[i], t[i + 1]);
+	}
+
+	return t;
+}
+
+string unSubstitution(string t)
+{
+	int n;
+	int l = t.length();
+
+	//iterate all posible keys
+	for (int j = 1; j < 26; j++)
+	{
+		cout << endl << "klucz = " << j << endl;
+		//iterate string array
+		for (int i = 0; i < l; i++)
+		{
+			//check if the current char is in the uppercase range
+			if (t[i] >= 'A' && t[i] <= 'Z')
+			{
+				//check if char doesn't need to be transferred 
+				if (t[i] + j <= 'Z')
+				{
+					cout << char(t[i] + j);
+				}
+				else {
+					cout << char(t[i] + j - 26);
+				}
+			}
+			//check if the current char is in the lowercase range
+			else if (t[i] >= 'a' && t[i] <= 'z')
+			{
+				//check if char doesn't need to be transferred 
+				if (t[i] + j <= 'z')
+				{
+					cout << char(t[i] + j);
+				}
+				else {
+					cout << char(t[i] + j - 26);
+				}
+			}
+			else {
+				//if the current char is out of range print the same char 
+				cout << char(t[i]);
+			}
+		}
+	}
+
+	cout << endl << "podaj poprawny klucz" << endl;
+	cin >> n;
+
+	t = substitution(t, n);
+
+	return t;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char option;
+	string text;
+	int key;
+
+	while (true)
+	{
+		cout << "co chcesz zrobić" << endl;
+		cout << "[1] szyfr podstawieniowy" << endl;
+		cout << "[2] szyfr przestawieniowy" << endl;
+		cout << "[3] szyfr podstawieniowo-podstawieniowy" << endl;
+		cout << "[4] odszyfruj" << endl;
+		cin >> option;
+
+		switch (option)
+		{
+		case '1':
+			cout << "podaj klucz [1 ---- 26]" << endl;
+			cin >> key;
+
+			cout << "podaj tekst do kodowania" << endl;
+			cin.ignore();
+			getline(cin, text);
+
+			text = substitution(text, key);
+
+			cout << text << endl;
+			break;
+		case '2':
+			cout << "podaj tekst do kodowania" << endl;
+			cin.ignore();
+			getline(cin, text);
+
+			text = transposition(text);
+
+			cout << text << endl;
+			break;
+		case '3':
+			cout << "podaj klucz [1 ---- 26]" << endl;
+			cin >> key;
+
+			cout << "podaj tekst do kodowania" << endl;
+			cin.ignore();
+			getline(cin, text);
+
+			text = substitution(transposition(text), key);
+
+			cout << text << endl;
+			break;
+		case '4':
+			cout << "podaj tekst do odkodowania" << endl;
+			cin.ignore();
+			getline(cin, text);
+
+			text = unSubstitution(text);
+
+			cout << text << endl;
+			break;
+		default:
+			cout << "wybrano nieistniejącą opcje" << endl;
+			break;
+		}
+	}
+
+	return 0;
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
